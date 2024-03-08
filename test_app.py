cdb25849246b99ecc50a62528022ac41ae100e66
Hieu Nguyen <hieu.nguyen@hyneskhater.com>
2024-03-07 22:19:09 -0600

    flask unit tests

--- test_app.py
+++ test_app.py
@@ -32,7 +32,30 @@ def test_epochs_query_route():
 def test_specific_epoch_route():
     response1 = requests.get('http://127.0.0.1:5000/epochs')
     a_epoch = response1.json()[0]['EPOCH']
-    print(a_epoch)
     response2 = requests.get('http://127.0.0.1:5000/epochs/'+a_epoch)
     assert response2.status_code == 200
     assert isinstance(response2.json(), dict) == True
+
+def test_specific_epoch_speed_route():
+    response1 = requests.get('http://127.0.0.1:5000/epochs')
+    a_epoch = response1.json()[0]['EPOCH']
+    response2 = requests.get('http://127.0.0.1:5000/epochs/'+a_epoch+'/speed')
+    assert response2.status_code == 200
+    assert response2.json()['Speed (km/s)'] is not None
+    assert isinstance(response2.json(), dict) == True
+    
+def test_specific_epoch_route():
+    response1 = requests.get('http://127.0.0.1:5000/epochs')
+    a_epoch = response1.json()[0]['EPOCH']
+    response2 = requests.get('http://127.0.0.1:5000/epochs/'+a_epoch+'/location')
+    assert response2.status_code == 200
+    assert response2.json()['geolocation'] is not None
+    assert isinstance(response2.json(), dict) == True
+
+def test_specific_epoch_route():
+    response = requests.get('http://127.0.0.1:5000/now')
+    assert response.status_code == 200
+    assert response.json()['altitude'] is not None
+    assert response.json()['latitude'] is not None
+    assert response.json()['longitude'] is not None
+    assert response(response.json(), dict) == True
